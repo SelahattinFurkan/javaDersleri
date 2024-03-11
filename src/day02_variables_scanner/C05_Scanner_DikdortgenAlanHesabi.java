ee4d5c0e18f2fc49e37cdbd6134a04e6114c9822
SelahattinFurkan <154260536+SelahattinFurkan@users.noreply.github.com>
2024-03-11 13:35:03 +0300

    ilkgonderi

---
+++ src/day02_variables_scanner/C05_Scanner_DikdortgenAlanHesabi.java
@@ -0,0 +1,20 @@
+package day02_variables_scanner;
+
+import java.util.Scanner;
+
+public class C05_Scanner_DikdortgenAlanHesabi {
+
+    public static void main(String[] args) {
+
+        //Soru 4- Kullanicidan bir dikdortgenin 2 kenar uzunlugunu alip,
+        //        dikdortgenin alanini yazdirin.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen dikdortgenin kenar uzunlarini giriniz...");
+
+        double kenar1 = scanner.nextDouble();
+        double kenar2 = scanner.nextDouble();
+
+        System.out.println("Dikdortgenin alani =" + kenar1*kenar2);
+    }
+}
---
+++ src/day03_scanner_dataCasting/C02_Swap.java
@@ -0,0 +1,29 @@
+package day03_scanner_dataCasting;
+
+import java.util.Scanner;
+
+public class C02_Swap {
+
+    public static void main(String[] args) {
+
+        //Soru 7 (Interview)- Kullanicidan iki sayi alip ikisinin degerlerini degistirin(swap).
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen 1.sayiyi giriniz...");
+        int sayi1 = scanner.nextInt();
+
+        System.out.println("Lutfen 2.sayiyi giriniz...");
+        int sayi2 = scanner.nextInt();
+
+        int bosKova = 0;
+
+        bosKova = sayi1 ;
+        sayi1 = sayi2;
+        sayi2 = bosKova;
+
+        System.out.println("sayilarin yerini degistirdim " +
+                "\nSayi1 : " + sayi1 + ", Sayi2 : " + sayi2 + " oldu.");
+
+    }
+}
