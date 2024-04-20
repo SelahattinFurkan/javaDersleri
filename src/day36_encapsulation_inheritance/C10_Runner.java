b70bc1846cd1f546dae3fcf196c234d492a8d051
SelahattinFurkan <154260536+SelahattinFurkan@users.noreply.github.com>
2024-04-20 19:15:40 +0300

    day33

---
+++ src/day36_encapsulation_inheritance/C10_Runner.java
@@ -0,0 +1,26 @@
+package day36_encapsulation_inheritance;
+
+public class C10_Runner {
+
+    public static void main(String[] args) {
+
+
+        C09_Corolla corolla = new C09_Corolla();
+
+        System.out.println(corolla.marka);
+        System.out.println(corolla.model);
+        System.out.println(corolla.yakit);
+        System.out.println(corolla.yil);
+        System.out.println(corolla.uretimYeri);
+        System.out.println(corolla.aku);
+        System.out.println(corolla.guvenlik);
+        System.out.println(corolla.kasa);
+        System.out.println(corolla.teker);
+        System.out.println(corolla.motor);
+
+
+    }
+
+}
+
+
